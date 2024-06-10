f76ae833a7e19e5c0d168f1814863887e7884fb5
unknown <alidev1414@gmail.com>
2024-06-10 20:01:38 +0900

    add: products detail page

--- controllers/mainProdcuts.controller.js
+++ controllers/mainProdcuts.controller.js
@@ -1,8 +1,34 @@
 const Product = require("../models/products.model")
+const fs = require("fs")
+const path = require("path")
+const dataPath = path.join(__dirname, "../data/products.json")
 
+// products page
 const getMainProduct = (req, res) => {
     const products = Product.findAll()
     res.render("products", { products })
 }
 
-module.exports = {getMainProduct}
+// detail page
+const getDetailProduct = (req, res) => {
+    const productId = req.params.uid;
+    
+    fs.readFile(dataPath, 'utf8', (err, data) => {
+      if (err) {
+        return res.status(500).render("500", { title: "Server Error" });
+      }
+      const products = JSON.parse(data);
+      const product = products.find(product => product.uid === productId);
+      
+      if (!product) {
+        return res.status(404).render("404", { title: "User Not Found" });
+      }
+      
+      res.render("productsMoreInfo", {
+        title: "Product Details",
+        product: product
+      });
+    });
+  }
+
+module.exports = {getMainProduct, getDetailProduct}
--- routes/mainProducts.js
+++ routes/mainProducts.js
@@ -1,8 +1,12 @@
 const express = require("express")
-const { getMainProduct } = require("../controllers/mainProdcuts.controller")
+const { getMainProduct, getDetailProduct } = require("../controllers/mainProdcuts.controller")
 
 const router = express.Router()
 
+// products page
 router.get("/", getMainProduct)
+
+// detail page
+router.get("/detail/:uid", getDetailProduct)
 
 module.exports = router
